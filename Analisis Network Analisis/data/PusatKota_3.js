f416f23a3b421351665989b99bbc07ea7b1fe510
uuyyyun <164850322+uuyyyun@users.noreply.github.com>
2024-06-24 20:20:43 +0700

    Analisis Spasial

---
+++ Analisis Network Analisis/data/PusatKota_3.js
@@ -0,0 +1,1 @@
+var json_PusatKota_3 = {"type":"FeatureCollection","name":"PusatKota_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"nama":"Alun - Alun Selatan Surakarta"},"geometry":{"type":"Point","coordinates":[110.826412813495324,-7.582140856751715]}}]}
